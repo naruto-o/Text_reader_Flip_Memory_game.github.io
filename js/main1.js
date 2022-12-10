3d8b22b39f70db6e03460b4884e4757f94d316ba
Shikhar Sharma <73755581+naruto-o@users.noreply.github.com>
2022-12-10 15:38:33 +0530

    Add files via upload

---
+++ js/main1.js
@@ -0,0 +1,9 @@
+function myFunction(){
+    document.getElementById("title").innerHTML="Harry Porter";
+}
+function myFunction1(){
+    document.getElementById("title").innerHTML="Alice In Wonderland";
+}
+function myFunction2(){
+    document.getElementById("title").innerHTML="Game Of Thrones";
+}
